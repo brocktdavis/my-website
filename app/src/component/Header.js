ac9730c6af222e75096a67513faecbb27e4d4cd4
brock <btdavis9999@gmail.com>
2022-07-19 21:51:47 -0500

    Add banner component

---
+++ app/src/component/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul className='banner'>
+          <li>Button 1</li>
+          <li>Button 2</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Header;
