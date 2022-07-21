7a4504917d7688da966451d083883801db20cf01
brock <btdavis9999@gmail.com>
2022-07-20 20:00:39 -0500

    Added banner link and styling

--- app/src/component/Header.js
+++ app/src/component/Header.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import './Header.css'
+
+
+function BannerLink(props) {
+  return (
+    <li className='banner-link' onClick={props.onClick}>
+      <p>{props.text}</p>
+    </li>
+  );
+}
 
 class Header extends React.Component {
   render() {
     return (
       <div>
         <ul className='banner'>
-          <li>Button 1</li>
-          <li>Button 2</li>
+          <BannerLink
+            onClick={() => alert('banner click!')}
+            text="BannerLink1"
+          />
+          <BannerLink
+            onClick={() => alert('banner click2!')}
+            text="BannerLink2"
+          />
         </ul>
+        <br className='banner-break' />
       </div>
     );
   }
