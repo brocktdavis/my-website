1765f3ef07bc772ed19b4acc22f3bce34e58ab54
Brock Davis <btdavis9999@gmail.com>
2023-11-13 22:56:08 -0600

    Begin navigation options in header with built-in components

--- app/src/root/Header.js
+++ app/src/root/Header.js
@@ -3,8 +3,10 @@ import {
     Outlet,
     useLocation,
 } from 'react-router-dom';
-import { Container } from '@mui/material';
+import { AppBar, Box, Button, Container, IconButton, Toolbar, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { Menu } from '@mui/icons-material';
 
+/** Tracks the router (i.e. URL) location. */
 const useRouteTracking = () => {
   const location = useLocation();
 
@@ -13,22 +15,60 @@ const useRouteTracking = () => {
   }, [location])
 };
 
+/** Hook that returns `true` if the navigation options should be collapsed into a presented menu,
+ *  rather than directly on the App Bar (e.g. for mobile phones with less toolbar space) */
+const useShouldCollapseHeader = () => {
+  const theme = useTheme();
+  return useMediaQuery(theme.breakpoints.down('sm'));
+};
+
+/** Renders navigation options as a menu button that opens a presented modal-type side container. */
+const NavigationSideMenu = () => {
+
+  return (
+    <IconButton
+      size='large'
+      edge='start'
+      color='inherit'
+      aria-label='menu'
+      sx={{ mr: 2 }}
+    >
+      <Menu />
+    </IconButton>
+  );
+};
+
+/** Renders navigation options directly on the toolbar. */
+const NavigationBar = () => {
+
+  return (
+    <>
+      <Button color='inherit'>Option 1</Button>
+      <Button color='inherit'>Option 2</Button>
+      <Button color='inherit'>Option 3</Button>
+    </>
+  );
+};
+
 export const Header = () => {
   useRouteTracking();
 
+  const shouldCollapseOptions = useShouldCollapseHeader();
+
   return (
     <div id="header">
-      <Container
-        sx={{
-          width: 1,
-          height: 100,
-          bgcolor: 'primary.main',
-          border: '1px solid transparent', // forces children inside box
-        }}
-        maxWidth={false}
-      >
-        <p>Hello, Header</p>
-      </Container>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position='static'>
+          <Toolbar>
+            { shouldCollapseOptions ? (
+              <NavigationSideMenu />
+            ) : (
+              <NavigationBar />
+            )}
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <Outlet />
     </div>
   );
