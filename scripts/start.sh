26733b8fbeef580e4284dcc59627b728ca5aa72d
Brock Davis <btdavis9999@gmail.com>
2024-12-20 01:22:19 -0600

    Update docker files

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,3 @@
 # Use this script to start the docker containers
 
 docker compose up -d --build
-
-# Copy node modules from src container to host filesystem
-docker cp "$(docker compose ps -q reactjs)":/reactjs/node_modules ./reactjs
