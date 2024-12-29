01ac4d149eddca9489706f59a5064c541718f281
Brock Davis <btdavis9999@gmail.com>
2024-12-29 12:28:05 -0600

    Work in progress

--- reactjs/src/pages/albion/ui/Albion.tsx
+++ reactjs/src/pages/albion/ui/Albion.tsx
@@ -1,8 +1,33 @@
 
+type PlayerPreviewProps = {
+  name: string,
+}
+
+const PlayerPreview = (props: PlayerPreviewProps) => (
+  <div className=''>
+    <p>{props.name}</p>
+  </div>
+);
+
+const PREVIEW_DATA = [
+  { name: 'P1' },
+];
+
 export const AlbionPage = () => {
   return (
-    <div>
+    <div className='container mx-auto m-4 border border-white'>
       AlbionPage
+      <div className='flex flex-row'>
+        <div>
+          <p>Players</p>
+          { PREVIEW_DATA.map(datum => (
+            <PlayerPreview name={datum.name} />
+          ))}
+        </div>
+        <div className='flex-1 border border-red-600'>
+          col 2
+        </div>
+      </div>
     </div>
   );
 };
